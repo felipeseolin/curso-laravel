16ba4dce980a182d9983e10bcdd2d3497697a815
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-05-14 16:33:18 -0300

    Aula 4 - Rotas

--- laravel53-basico/routes/web.php
+++ laravel53-basico/routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
+Route::get('/nome/nome2/nome3', function () {
+	return 'Rota grande';
+})->name('rota.nomeada');
+
+Route::any('/any', function () {
+	return 'Route any';
+});
+
+Route::match(['get', 'post'], '/match', function () {
+	return 'Route match';
+});
+
+Route::post('/post', function (){
+	return 'Route Post';
+});
+
+Route::get('/contato', function (){
+	return 'Contato';
+});
+
+Route::get('/empresa', function () {
+	return view('empresa');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('rota.nomeada');
 });
